5b062054f07919f438946b29359bfaca089613a8
David Chung <dbchung3@yahoo.com>
2020-04-14 01:49:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -28,9 +28,10 @@ def return_string_array(array)
   counter = 0 
   new_array = []
   while counter < array.length do 
-    array[count].to_s 
+    new_array.push(array[count].to_s)
     counter +=1
   end 
+  new_array
 end 
 
     
